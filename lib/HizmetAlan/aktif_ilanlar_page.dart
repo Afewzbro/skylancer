dcf7e964e2dbfe754fb6b42efd01e832c8e081c5
Testi Abdulwasi <76836235+Afewzbro@users.noreply.github.com>
2023-06-18 18:45:53 -0400

    aktif ilanlar sayfasi eklendi

---
+++ lib/HizmetAlan/aktif_ilanlar_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AktifIlanlarPage extends StatefulWidget {
+  const AktifIlanlarPage({super.key});
+
+  @override
+  State<AktifIlanlarPage> createState() => _AktifIlanlarPageState();
+}
+
+class _AktifIlanlarPageState extends State<AktifIlanlarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
