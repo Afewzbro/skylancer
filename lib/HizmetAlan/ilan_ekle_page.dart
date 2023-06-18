f730c21c21b1b9bbb739cb821da404de284ed6a5
Testi Abdulwasi <76836235+Afewzbro@users.noreply.github.com>
2023-06-18 18:46:27 -0400

    ilan ekle sayfasi eklendi

---
+++ lib/HizmetAlan/ilan_ekle_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IlanEklePage extends StatefulWidget {
+  const IlanEklePage({super.key});
+
+  @override
+  State<IlanEklePage> createState() => _IlanEklePageState();
+}
+
+class _IlanEklePageState extends State<IlanEklePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
