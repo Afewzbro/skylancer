08a07a50a3326883ef31aca4e2ad614092494170
Testi Abdulwasi <76836235+Afewzbro@users.noreply.github.com>
2023-06-18 18:37:25 -0400

    Delete lib/constants directory
    güncellenen uygulama tasarımına göre bunun kaldırılması gerekiyor

--- lib/constants/routes.dart
+++
@@ -1,7 +0,0 @@
-const loginRoute = '/login/';
-const registerPage1Route = '/register1/';
-const hizmetAlRegisterRoute = '/hizmetAlRegister/';
-const hizmetVerRegister1Route = '/hizmetVerRegister1/';
-const hizmetVerRegister2Route = '/hizmetVerRegister2/';
-const verifyEmailRoute = '/verify-email/';
-const createOrUpdateNoteRoute = '/notes/new-note';
